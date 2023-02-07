1e43f69a8c79d97d94cb23264e701935197dda67
Ngô Vưn Nam <anh02284@gmail.com>
2023-02-07 16:45:24 +0700

    change origin header

--- src/background.js
+++ src/background.js
@@ -1,8 +1,40 @@
+
 chrome.action.onClicked.addListener(tab => {
-    // console.log('tab',tab);
     const url = chrome.runtime.getURL("popup.html");
     console.log('url',url)
     chrome.tabs.create({
         url
       });
+      updateDynamicRules(url, 'https://www.facebook.com/')
 })
+function updateDynamicRules(headerKey, headerValue) {
+  chrome.declarativeNetRequest.updateDynamicRules({
+    addRules: [
+      {
+        id: 1,
+        priority: 1,
+        action: {
+          type: 'modifyHeaders',
+          requestHeaders: [
+            { 
+              header: headerKey, 
+              operation: 'set', 
+              value: headerValue
+            },
+          ],
+        },
+        condition: {
+          regexFilter: '|http*',
+          resourceTypes: [
+            'main_frame',
+            'sub_frame',
+            'script'
+          ],
+        },
+      },
+    ],
+    removeRuleIds: [1]
+  }, async (result) => {
+    console.log('created', result);
+  });
+}
