ed9dfedaa38bf208edbffc3dcd5e2642c5ea3c50
Ngô Vưn Nam <anh02284@gmail.com>
2023-02-08 13:28:53 +0700

    change origin header

--- src/background.js
+++ src/background.js
@@ -1,40 +1,21 @@
-
-chrome.action.onClicked.addListener(tab => {
-    const url = chrome.runtime.getURL("popup.html");
-    console.log('url',url)
-    chrome.tabs.create({
-        url
-      });
-      updateDynamicRules(url, 'https://www.facebook.com/')
-})
-function updateDynamicRules(headerKey, headerValue) {
-  chrome.declarativeNetRequest.updateDynamicRules({
+chrome.declarativeNetRequest.updateDynamicRules({
     addRules: [
-      {
-        id: 1,
-        priority: 1,
-        action: {
-          type: 'modifyHeaders',
-          requestHeaders: [
-            { 
-              header: headerKey, 
-              operation: 'set', 
-              value: headerValue
+        {
+            id: 1,
+            priority: 1,
+            action: {
+                type: 'modifyHeaders',
+                requestHeaders: [{ header: 'origin', operation: 'set', value: 'https://www.facebook.com' }],
             },
-          ],
+            condition: { urlFilter: 'facebook.com', resourceTypes: ['xmlhttprequest'] },
         },
-        condition: {
-          regexFilter: '|http*',
-          resourceTypes: [
-            'main_frame',
-            'sub_frame',
-            'script'
-          ],
-        },
-      },
     ],
-    removeRuleIds: [1]
-  }, async (result) => {
-    console.log('created', result);
-  });
-}
+    removeRuleIds: [1],
+});
+chrome.action.onClicked.addListener((tab) => {
+    const url = chrome.runtime.getURL('popup.html');
+    console.log('url', url);
+    chrome.tabs.create({
+        url,
+    });
+});
